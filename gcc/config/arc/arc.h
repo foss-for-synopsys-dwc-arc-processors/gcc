60d07e6092a8e179e840864e82762d5789f87eee
Jeremy Bennett <jeremy.bennett@embecosm.com>
2013-03-28 14:23:19 +0000

    2013-03-16  Jeremy Bennett  <jeremy.bennett@embecosm.com>
    	* config/arc/arc.h (DRIVER_SELF_SPECS): Insert space before
    	concatenated -mcpu options.

--- gcc/config/arc/arc.h
+++ gcc/config/arc/arc.h
@@ -211,10 +211,10 @@ along with GCC; see the file COPYING3.  If not see
 #endif
 
 #define DRIVER_SELF_SPECS DRIVER_ENDIAN_SELF_SPECS \
-  "%{mARC5:-mcpu=A5 %<mA5}" \
-  "%{mARC600|mA6:-mcpu=ARC600 %<mARC600}" \
-  "%{mARC601:-mcpu=ARC601 %<mARC601}" \
-  "%{mARC700|mA7:-mcpu=ARC700 %<mARC700}"
+  "%{mARC5: -mcpu=A5 %<mA5}" \
+  "%{mARC600|mA6: -mcpu=ARC600 %<mARC600}" \
+  "%{mARC601: -mcpu=ARC601 %<mARC601}" \
+  "%{mARC700|mA7: -mcpu=ARC700 %<mARC700}"
 
 /* Run-time compilation parameters selecting different hardware subsets.  */
 
