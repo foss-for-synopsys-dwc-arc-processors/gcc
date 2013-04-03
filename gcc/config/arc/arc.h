22683246d9dd50cc82bbe3fd5e589367ca271936
Joern Rennecke <joern.rennecke@embecosm.com>
2013-04-03 19:54:56 +0100

    * config/arc/arc.h (DRIVER_SELF_SPECS): Also remove -mA6 / -mA7.

--- gcc/config/arc/arc.h
+++ gcc/config/arc/arc.h
@@ -218,9 +218,9 @@ along with GCC; see the file COPYING3.  If not see
 
 #define DRIVER_SELF_SPECS DRIVER_ENDIAN_SELF_SPECS \
   "%{mARC5: -mcpu=A5 %<mA5}" \
-  "%{mARC600|mA6: -mcpu=ARC600 %<mARC600}" \
+  "%{mARC600|mA6: -mcpu=ARC600 %<mARC600 %<A6}" \
   "%{mARC601: -mcpu=ARC601 %<mARC601}" \
-  "%{mARC700|mA7: -mcpu=ARC700 %<mARC700}"
+  "%{mARC700|mA7: -mcpu=ARC700 %<mARC700 %<mA7}"
 
 /* Run-time compilation parameters selecting different hardware subsets.  */
 
