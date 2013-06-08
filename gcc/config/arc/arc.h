94491a65928141db2ef9b13ca2eb3064fa66ea51
Joern Rennecke <joern.rennecke@embecosm.com>
2013-06-08 12:32:51 +0100

    Add -mARC600 -mnorm support for TARGET_OPTFPE; small fixes for the other TARGET_OPTFPE subtargets. gcc:         * config/arc/arc.h (TARGET_OPTFPE): No longer require a multiplier,         but require a barrel shifter. libgcc:         * config/arc/dp-hack.h         [!__ARC700__ && !__ARC_MUL64__ && !__ARC_MUL32BY16__] (QUIET_NAN):         Define to 0xfffffffffffffLL.         [!__ARC700__ && !__ARC_MUL64__ && !__ARC_MUL32BY16__] (__muldf3):         define as __muldf3_c.         [!__ARC700__ && !__ARC_MUL64__ && !__ARC_MUL32BY16__] (__divdf3):         define as __divdf3_c.         * config/arc/ieee-754/arc600-dsp/muldf3.S (__muldf3):         Add FUNC/ENDFUNC.         * config/arc/ieee-754/arc600: Rename directory to ..         * config/arc/ieee-754/arc600-mul64: .. this.         * config/arc/ieee-754/arc600-mul64/mulsf3.S         (.Ldenorm_dbl0_inf_nan_dbl1): Fix sign of subnormal*inf.         * config/arc/ieee-754/arc600: New directory.         * config/arc/lib1funcs.S: Reflect arc600-mul64 directory rename.         [ARC_NORM]: Include ieee-754/arc600/mulsf3.S, ieee-754/arc600/divsf3.S.         * config/arc/t-arc b/libgcc/config/arc/t-arc (dp-bit.c):         Place dp-hack.h include after fp-bit.h include.

--- gcc/config/arc/arc.h
+++ gcc/config/arc/arc.h
@@ -270,8 +270,8 @@ along with GCC; see the file COPYING3.  If not see
 /* Indicate if an optimized floating point emulation library is available.  */
 #define TARGET_OPTFPE \
  (TARGET_ARC700 \
-  || ((TARGET_ARC600 || TARGET_ARC601) && TARGET_NORM_SET \
-      && (TARGET_MUL64_SET || TARGET_MULMAC_32BY16_SET)))
+  /* We need a barrel shifter and NORM.  */ \
+  || (TARGET_ARC600 && TARGET_NORM_SET))
 
 /* Non-zero means the cpu supports swap instruction.  This flag is set by
    default for A7, and only for pre A7 cores when -mswap is given.  */
