64cb5313d1b983525a1820396ed62db02c35ec7c
Claudiu Zissulescu <claziss@synopsys.com>
2013-07-02 17:23:29 +0200

    Ignore tests for V1

--- gcc/testsuite/gcc.target/arc/builtin_fls.c
+++ gcc/testsuite/gcc.target/arc/builtin_fls.c
@@ -1,9 +1,9 @@
 /* Check that we use the fls insn by checking assembler output. */
 /* { dg-do compile } */
-/* { dg-xfail-if "Not available for ARCv1" { *-*-* } { "-mA7" "-mA5" "-mA6" "-mARC600" "-mARC700" "-mcpu=A5" "-mcpu=A600" "-mcpu=A601" "-mcpu=A700" } { "" } } */
+/* { dg-skip-if "Not available for ARCv1" { *-*-* } { "-mA7" "-mA5" "-mA6" "-mARC600" "-mARC700" "-mcpu=A5" "-mcpu=A600" "-mcpu=A601" "-mcpu=A700" } { "" } } */
 /* { dg-options "-O2 -mnorm" } */
-/* { dg-final { scan-assembler "fls.*r0, r0" } } */
-/* { dg-final { scan-assembler "fls.*r0, 16" } } */
+/* { dg-final { scan-assembler "\tfls.*r0, r0" } } */
+/* { dg-final { scan-assembler "\tfls.*r0, 16" } } */
 int foo (int x)
 {
   return __builtin_arc_fls(x);
--- gcc/testsuite/gcc.target/arc/builtin_kflag.c
+++ gcc/testsuite/gcc.target/arc/builtin_kflag.c
@@ -1,9 +1,9 @@
 /* Check that we use the clri insn by checking assembler output. */
 /* { dg-do compile } */
-/* { dg-xfail-if "Not available for ARCv1" { *-*-* } { "-mA7" "-mA5" "-mA6" "-mARC600" "-mARC700" "-mcpu=A5" "-mcpu=A600" "-mcpu=A601" "-mcpu=A700" } { "" } } */
-/* { dg-options "-O2 -mEM" } */
-/* { dg-final { scan-assembler "kflag.ne.*r0" } } */
-/* { dg-final { scan-assembler "kflag.*16" } } */
+/* { dg-skip-if "Not available for ARCv1" { *-*-* } { "-mA7" "-mA5" "-mA6" "-mARC600" "-mARC700" "-mcpu=A5" "-mcpu=A600" "-mcpu=A601" "-mcpu=A700" } { "" } } */
+/* { dg-options "-O2" } */
+/* { dg-final { scan-assembler "\tkflag.ne.*r0" } } */
+/* { dg-final { scan-assembler "\tkflag.*16" } } */
 void foo (int x)
 {
   if (x)
