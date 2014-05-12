1e5f17907dcddb7b7b114b7f9febb1ee1efcd22e
Joern Rennecke <joern.rennecke@embecosm.com>
2014-05-12 17:34:02 +0100

    * config/arc/arc-protos.h (arc_eh_uses): Prototype.         * config/arc/arc.c (arc_eh_uses): New function.         * config/arc/arc.h (EH_USES): Define.

--- gcc/config/arc/arc.h
+++ gcc/config/arc/arc.h
@@ -960,6 +960,8 @@ arc_return_addr_rtx(COUNT,FRAME)
 
 #define EPILOGUE_USES(REGNO) arc_epilogue_uses ((REGNO))
 
+#define EH_USES(REGNO) arc_eh_uses((REGNO))
+
 /* Definitions for register eliminations.
 
    This is an array of structures.  Each structure initializes one pair
