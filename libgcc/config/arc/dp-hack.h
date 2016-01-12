0ef32da9c6146ffd7e523e82ae8dcfd3cfca94df
Claudiu Zissulescu <claziss@synopsys.com>
2016-01-12 17:56:14 +0100

    Bug fixes: use assembly routines only when multiplier is present.
    ARCHS doesn't have by default a configuration with the multiplier block
    present.  Take this into account. Side effect: the ARCEM with multiplier
    block also uses the accelerated soft floating point implementations.
    This patch solves STAR#9000950193.
    
    libgcc/
    2016-01-12  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* config/arc/dp-hack.h: Use accelerated routines only multiplier
    	is present.
    	* config/arc/ieee-754/divdf3.S: Likewise.
    	* config/arc/lib1funcs.S: Likewise.

--- libgcc/config/arc/dp-hack.h
+++ libgcc/config/arc/dp-hack.h
@@ -30,72 +30,77 @@ see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see
 
 #define FINE_GRAINED_LIBRARIES
 #define ARC_DP_DEBUG 1
+
 #if !defined (__ARC_NORM__) || ARC_DP_DEBUG
-#define L_pack_df
-#define L_unpack_df
-#define L_make_df
-#define L_thenan_df
-#define L_sf_to_df
+# define L_pack_df
+# define L_unpack_df
+# define L_make_df
+# define L_thenan_df
+# define L_sf_to_df
 #endif
+
 #ifndef __ARC_NORM__
-#define L_addsub_df
+# define L_addsub_df
 #elif ARC_DP_DEBUG
-#define L_addsub_df
-#define __adddf3 __adddf3_c
-#define __subdf3 __subdf3_c
+# define L_addsub_df
+# define __adddf3 __adddf3_c
+# define __subdf3 __subdf3_c
 #endif
+
 #ifndef __ARC_NORM__
-#define L_mul_df
-#define L_div_df
+# define L_mul_df
+# define L_div_df
 #elif (!defined (__ARC700__) && !defined (__ARC_MUL64__) \
-       && !defined(__ARC_MUL32BY16__) && !defined (__HS__))
-#define L_mul_df
-#define L_div_df
-#undef QUIET_NAN
-#define QUIET_NAN 0xfffffffffffffLL
+       && !defined(__ARC_MUL32BY16__) && !defined (__ARC_MPY__))
+# define L_mul_df
+# define L_div_df
+# undef QUIET_NAN
+# define QUIET_NAN 0xfffffffffffffLL
 #elif ARC_DP_DEBUG
-#define L_mul_df
-#define __muldf3 __muldf3_c
-#define L_div_df
-#define __divdf3 __divdf3_c
+# define L_mul_df
+# define __muldf3 __muldf3_c
+# define L_div_df
+# define __divdf3 __divdf3_c
 #endif
+
 #ifndef __ARC_NORM__
-#define L_df_to_sf
-#define L_si_to_df
-#define L_df_to_si
-#define L_tf_to_usi /* need to defined this instead of df_to_usi */
-#define L_usi_to_df
+# define L_df_to_sf
+# define L_si_to_df
+# define L_df_to_si
+# define L_tf_to_usi /* need to defined this instead of df_to_usi */
+# define L_usi_to_df
 #elif ARC_DP_DEBUG
-#define L_df_to_sf
-#define __truncdfsf2 __truncdfsf2_c
-#define L_si_to_df
-#define __floatsidf __floatsidf_c
-#define L_df_to_si
-#define __fixdfsi __fixdfsi_c
-#define L_tf_to_usi
-#define __fixunsdfsi __fixunsdfsi_c
-#define L_usi_to_df
-#define __floatunsidf __floatunsidf_c
+# define L_df_to_sf
+# define __truncdfsf2 __truncdfsf2_c
+# define L_si_to_df
+# define __floatsidf __floatsidf_c
+# define L_df_to_si
+# define __fixdfsi __fixdfsi_c
+# define L_tf_to_usi
+# define __fixunsdfsi __fixunsdfsi_c
+# define L_usi_to_df
+# define __floatunsidf __floatunsidf_c
 #endif
+
 #ifndef __ARC_NORM__
-#define L_fpcmp_parts_df
-#define L_compare_df
-#define L_eq_df
-#define L_ne_df
-#define L_gt_df
-#define L_ge_df
-#define L_lt_df
-#define L_le_df
-#define L_unord_df
-#define L_negate_df
+# define L_fpcmp_parts_df
+# define L_compare_df
+# define L_eq_df
+# define L_ne_df
+# define L_gt_df
+# define L_ge_df
+# define L_lt_df
+# define L_le_df
+# define L_unord_df
+# define L_negate_df
 #elif ARC_DP_DEBUG
-#define L_fpcmp_parts_df
-#define L_eq_df
-#define __eqdf2 __eqdf2_c
-#define L_gt_df
-#define __gtdf2 __gtdf2_c
-#define L_ge_df
-#define __gedf2 __gedf2_c
-#define L_unord_df
-#define __unorddf2 __unorddf2_c
+# define L_fpcmp_parts_df
+# define L_eq_df
+# define __eqdf2 __eqdf2_c
+# define L_gt_df
+# define __gtdf2 __gtdf2_c
+# define L_ge_df
+# define __gedf2 __gedf2_c
+# define L_unord_df
+# define __unorddf2 __unorddf2_c
 #endif
