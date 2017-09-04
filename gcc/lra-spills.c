10edbfb25980cac3bfa9d64437f21775207ed30b
claziss <claziss@synopsys.com>
2017-09-04 12:37:38 +0200

    [MAINLINE][ARC] Fix choosing spill register.
    LRA assigns a spill hard register (assign_spill_hard_regs) to a pseudo
    by only checking if a hard-register is overlaping a register from
    conflict_hard_regs.  However, this assumption may not be correct as we
    can have the following situation: R0 R1 R2 are three hard registers,
    but R1 and R2 are free to allocate, and only R0R1 register pair can be
    of mode DImode. Thus, relaying only on the overlaping test, it may
    result that we can have a valid spill register in the R1R2 pair which
    is not allrigth.
    
    This proble is observed when testing vshuf-v8df.c for hs38_linux,
    bigendian configuration.
    
    gcc/
    2017-09-04  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* lra-spills.c (assign_spill_hard_regs): Check if the spill
    	register is valid for given mode.

--- gcc/lra-spills.c
+++ gcc/lra-spills.c
@@ -284,7 +284,8 @@ assign_spill_hard_regs (int *pseudo_regnos, int n)
       for (k = 0; k < spill_class_size; k++)
 	{
 	  hard_regno = ira_class_hard_regs[spill_class][k];
-	  if (! overlaps_hard_reg_set_p (conflict_hard_regs, mode, hard_regno))
+	  if (! overlaps_hard_reg_set_p (conflict_hard_regs, mode, hard_regno)
+	      && targetm.hard_regno_mode_ok (hard_regno, mode))
 	    break;
 	}
       if (k >= spill_class_size)
