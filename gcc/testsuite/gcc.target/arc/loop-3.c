5ed3c88df5cdfb10e46d4f76bcf10b643d622fcb
Claudiu Zissulescu <claziss@gmail.com>
2017-12-15 18:42:06 +0200

    [ARC] Make lp_count register special.
    LP_COUNT register cannot be freely allocated by the compiler as it
    size, and/or content may change depending on the ARC hardware
    configuration. Thus, make this register fixed.
    
    [FIX] [ZOL] reload pattern update
    [FIX][ZOL] Don't delete loop_end, confuses data analysis in dbr
    [FIX][ZOL] update.
    [FIX] ZOL reload pattern dbnz
    
    gcc/
    2017-12-05  Claudiu Zissulescu  <claziss@synopsys.com>
    
    	* config/arc/arc.c (hwloop_optimize): Update rtx patterns
    	involving lp_count register.
    	(arc_register_move_cost): Remove lp_count register related costs.
    	* config/arc/arc.h (FIXED_REGISTERS): Make lp_count fixed.
    	* config/arc/arc.md (movqi_insn): Remove unsed lp_count
    	constraints.
    	(movhi_insn): Likewise.
    	(movsi_insn): Update pattern.
    	(arc_lp): Likewise.
    	(dbnz): Likewise.
    	* config/arc/constraints.md (l): Change it from register
    	constraint to constraint.

--- gcc/testsuite/gcc.target/arc/loop-3.c
+++ gcc/testsuite/gcc.target/arc/loop-3.c
@@ -23,5 +23,5 @@ void fn1(void)
   }
 }
 
-/* { dg-final { scan-assembler "bne_s @.L2" } } */
+/* { dg-final { scan-assembler "bne_s @.L2" { target { ! { arc700 || arc600 } } } } } */
 /* { dg-final { scan-assembler-not "add.eq" } } */
