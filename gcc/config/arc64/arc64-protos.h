1fc530952c96be4a631541a565716604bb114882
Claudiu Zissulescu <claziss@gmail.com>
2019-09-06 17:20:49 +0200

    Cleanup #1

--- gcc/config/arc64/arc64-protos.h
+++ gcc/config/arc64/arc64-protos.h
@@ -4,15 +4,18 @@
 extern int arc64_epilogue_uses (int);
 extern HOST_WIDE_INT arc64_initial_elimination_offset (unsigned, unsigned);
 extern void arc64_init_expanders (void);
-extern rtx arc64_return_addr (int, rtx);
-extern HOST_WIDE_INT arc64_regmode_natural_size (machine_mode);
+extern void arc64_cpu_cpp_builtins (cpp_reader *);
 
 #ifdef RTX_CODE
 
+extern rtx arc64_return_addr (int, rtx);
 extern machine_mode arc64_select_cc_mode (enum rtx_code, rtx, rtx);
 extern bool arc64_check_mov_const (HOST_WIDE_INT );
 extern bool arc64_split_mov_const (rtx *);
 extern bool arc64_can_use_return_insn_p (void);
+extern void arc64_expand_call (rtx, rtx, bool);
+extern rtx arc64_gen_compare_reg (enum rtx_code, rtx, rtx);
+extern bool arc64_prepare_move_operands (rtx, rtx, machine_mode);
 
 #endif /* RTX_CODE */
 
