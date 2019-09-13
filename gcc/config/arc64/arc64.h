9da731a2e8ee09e18bdd45a7235d6f26c7f25b9a
Shahab Vahedi <shahab@synopsys.com>
2019-09-13 15:58:30 +0200

    [ARCx] Fix SP popping issue and use movl for movdi
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

--- gcc/config/arc64/arc64.h
+++ gcc/config/arc64/arc64.h
@@ -226,7 +226,7 @@ enum reg_class
     { 0x00000000, 0x00000000, 0x00000000 }, /* NO_REGS */	\
     { 0x0000f00f, 0x00000000, 0x00000000 }, /* AC16_REGS */	\
     { 0x00001fff, 0x00000000, 0x00000000 }, /* SIBCALL_REGS */	\
-    { 0x4fffffff, 0x0fffffff, 0x00000000 }, /* GENERAL_REGS */	\
+    { 0x5fffffff, 0x0fffffff, 0x00000000 }, /* GENERAL_REGS */	\
     { 0xffffffff, 0xffffffff, 0x00000007 }, /* ALL_REGS */	\
    }
 
