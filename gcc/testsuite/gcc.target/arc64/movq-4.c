979994263faf811bfc32fd2eb2f6ea987b0ae1d8
Shahab Vahedi <shahab@synopsys.com>
2019-09-26 12:23:32 +0200

    [ARCx] break movq test
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

---
+++ gcc/testsuite/gcc.target/arc64/movq-4.c
@@ -0,0 +1,30 @@
+/* { dg-do compile } */
+/* { dg-options "-O0" } */
+
+/* "movqi" is about assigning 8-bit of data (byte).   *
+ *                                                    *
+ * This is one of the tests in series of testing for  *
+ * "movqi" support. The test scenarios are as the     *
+ * following table:                                   *
+ *                                                    *
+ * ,--------.----------.----------.-----------------. *
+ * | test   | notation | dest.    | source          | *
+ * |--------|---------------------------------------| *
+ * | movq-1 | r <- i   | register | immediate       | *
+ * | movq-2 | r <- r   |          | register        | *
+ * | movq-3 | r <- m   |          | memory          | *
+ * |--------+----------+----------+-----------------| *
+ * | movq-4 | m <- r   |          | register        | *
+ * | movq-5 | m <- i   | memroy   | immediate small | *
+ * | movq-6 | m <- I   |          | immediate big   | *
+ * | movq-7 | m <- m   |          | memory          | *
+ * `--------^----------^----------^-----------------' */
+
+/* register to memory */
+char mem;
+void foo(void)
+{
+  register char reg_char;
+  mem = reg_char;
+}
+/* { dg-final { scan-assembler "stb\\s+r\[0-9\]+,\\\[@mem\\\]" } } */
---
+++ gcc/testsuite/gcc.target/arc64/movq-6.c
@@ -0,0 +1,38 @@
+/* { dg-do compile } */
+/* { dg-options "-O0" } */
+
+/* "movqi" is about assigning 8-bit of data (byte).   *
+ *                                                    *
+ * This is one of the tests in series of testing for  *
+ * "movqi" support. The test scenarios are as the     *
+ * following table:                                   *
+ *                                                    *
+ * ,--------.----------.----------.-----------------. *
+ * | test   | notation | dest.    | source          | *
+ * |--------|---------------------------------------| *
+ * | movq-1 | r <- i   | register | immediate       | *
+ * | movq-2 | r <- r   |          | register        | *
+ * | movq-3 | r <- m   |          | memory          | *
+ * |--------+----------+----------+-----------------| *
+ * | movq-4 | m <- r   |          | register        | *
+ * | movq-5 | m <- i   | memroy   | immediate small | *
+ * | movq-6 | m <- I   |          | immediate big   | *
+ * | movq-7 | m <- m   |          | memory          | *
+ * `--------^----------^----------^-----------------' */
+
+///* assign immediate to a memory: this immediate cannot be   *
+// * represented by 6-bit, hence stb w6, mem is not an option */
+//char test06_mem;
+//void test06(void)
+//{
+//  test06_mem = 0x40;    /* smallest 7-bit number */
+//  test06_mem = 0x7F;    /* largest  7-bit number */
+//  test06_mem = 0x80;    /* smallest 8-bit number */
+//  test06_mem = 0xFF;    /* largest  8-bit number */
+//}
+/* FIXME: reverse gd so dejagnu would be triggered */
+/* { gd-final { scan-assembler "mov_s\\s+r2,64" } } */
+/* { gd-final { scan-assembler "mov_s\\s+r2,127" } } */
+/* { gd-final { scan-assembler "mov\\s+r2,-128" } } */
+/* { gd-final { scan-assembler "mov_s\\s+r2,-1" } } */
+/* { gd-final { scan-assembler-times "stb\\s+r2,\\\[@test06_mem\\\]" 3 } } */
