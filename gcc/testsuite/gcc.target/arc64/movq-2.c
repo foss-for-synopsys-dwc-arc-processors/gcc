8dd56618b880dbc747eefad3bece3db53a17eac8
Shahab Vahedi <shahab@synopsys.com>
2019-09-26 12:23:32 +0200

    [ARCx] break movq test
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

---
+++ gcc/testsuite/gcc.target/arc64/movq-2.c
@@ -0,0 +1,29 @@
+/* { dg-do compile } */
+/* { dg-options "-O0 -fomit-frame-pointer" } */
+
+/* "movqi" is about assigning 8-bit of data (byte).   *
+ *                                                    *
+ * This is one of the tests in series of testing for  *
+ * "movqi" support. The test scenarios are as the     *
+ * following table:                                   *
+ *                                                    *
+ * ,--------.----------.----------.-----------------. *
+ * | test   | notation | dest.    | source          | *
+ * |--------|---------------------------------------| *
+ * | movq-1 | r <- i   | register | immediate       | *
+ * | movq-2 | r <- r   |          | register        | *
+ * | movq-3 | r <- m   |          | memory          | *
+ * |--------+----------+----------+-----------------| *
+ * | movq-4 | m <- r   |          | register        | *
+ * | movq-5 | m <- i   | memroy   | immediate small | *
+ * | movq-6 | m <- I   |          | immediate big   | *
+ * | movq-7 | m <- m   |          | memory          | *
+ * `--------^----------^----------^-----------------' */
+
+/* assign a register to register */
+char foo(char a, char b)
+{
+  return b;
+}
+/* FIXME: figure out how to produce mov r,r */
+/* { dg-final { scan-assembler "mov\\s+r\[0-9\]+,r\[0-9\]+" } } */
---
+++ gcc/testsuite/gcc.target/arc64/movq-8.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-O0" } */
+
+/* "movqi" is about assigning 8-bit of data (byte).   *
+ *                                                    *
+ * This is one of the tests in series of testing for  *
+ * "movqi" support. The test scenarios are as the     *
+ * following table:                                   *
+ *                                                    *
+ * ,--------.----------.----------.-----------------. *
+ * | test   | notation | dest.    | source          | *
+ * |--------|---------------------------------------| *
+ * | movq-1 | r <- i   | register | immediate       | *
+ * | movq-2 | r <- r   |          | register        | *
+ * | movq-3 | r <- m   |          | memory          | *
+ * |--------+----------+----------+-----------------| *
+ * | movq-4 | m <- r   |          | register        | *
+ * | movq-5 | m <- i   | memroy   | immediate small | *
+ * | movq-6 | m <- I   |          | immediate big   | *
+ * | movq-7 | m <- m   |          | memory          | *
+ * `--------^----------^----------^-----------------' */
+
+/* FIXME: with a 'volatile' this test generates an unnecessary extb */
+/* assign memory to a memory */
+volatile char mem_dst, mem_src;
+void foo(void)
+{
+  mem_dst = mem_src;
+}
+/* { dg-final { scan-assembler "ldb\\s+r\[0-9\]+,\\\[@mem_src\\\]" } } */
+/* { dg-final { scan-assembler-not "ext\[bhw\]\\s+" } } */
+/* { dg-final { scan-assembler "stb\\s+r0,\\\[@mem_dst\\\]" } } */
