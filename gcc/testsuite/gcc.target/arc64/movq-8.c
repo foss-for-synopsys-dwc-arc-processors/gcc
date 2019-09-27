79b7918696ef89573bd15ada0955e270b96cae72
Shahab Vahedi <shahab@synopsys.com>
2019-09-27 09:40:23 +0200

    [ARCx] update in movq tests.
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

--- gcc/testsuite/gcc.target/arc64/movq-8.c
+++ gcc/testsuite/gcc.target/arc64/movq-8.c
@@ -1,24 +1,7 @@
 /* { dg-do compile } */
 /* { dg-options "-O0" } */
 
-/* "movqi" is about assigning 8-bit of data (byte).   *
- *                                                    *
- * This is one of the tests in series of testing for  *
- * "movqi" support. The test scenarios are as the     *
- * following table:                                   *
- *                                                    *
- * ,--------.----------.----------.-----------------. *
- * | test   | notation | dest.    | source          | *
- * |--------|---------------------------------------| *
- * | movq-1 | r <- i   | register | immediate       | *
- * | movq-2 | r <- r   |          | register        | *
- * | movq-3 | r <- m   |          | memory          | *
- * |--------+----------+----------+-----------------| *
- * | movq-4 | m <- r   |          | register        | *
- * | movq-5 | m <- i   | memroy   | immediate small | *
- * | movq-6 | m <- I   |          | immediate big   | *
- * | movq-7 | m <- m   |          | memory          | *
- * `--------^----------^----------^-----------------' */
+/* check "movq-1.c" for further details. */
 
 /* FIXME: with a 'volatile' this test generates an unnecessary extb */
 /* assign memory to a memory */
