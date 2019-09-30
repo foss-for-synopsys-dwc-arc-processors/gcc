3c8daa1c2e8b55a28973c1c8832b660331e0e939
Shahab Vahedi <shahab@synopsys.com>
2019-09-30 15:16:29 +0200

    [MD] Add movdi along with the tests
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

--- gcc/testsuite/gcc.target/arc64/movd-6.c
+++ gcc/testsuite/gcc.target/arc64/movd-6.c
@@ -1,11 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-O0 -fomit-frame-pointer" } */
 
-/* check "movq-1.c" for further details. */
+/* check "movd-1.c" for further details. */
 
 /* assign immediate to a memory: this immediate cannot be   *
  * represented by 6-bit, hence stb w6, mem is not an option */
-char mem;
+int mem;
 void foo(void)
 {
   mem = 0x40;    /* smallest 7-bit number */
@@ -13,11 +13,11 @@ void foo(void)
   mem = 0x80;    /* 128 */
   mem = -128;    /* smallest 8-bit number */
   mem = -33;     /* first negative number outside w6's range. else,
-                    it would use something like 'stb -32, [@mem]'  */
+                    it would use something like 'sth -32, [@mem]'  */
 }
 /* { dg-final { scan-assembler "mov_s\\s+r0,64" } } */
 /* { dg-final { scan-assembler "mov_s\\s+r0,127" } } */
 /* FIXME: mov r0,-128 is emitted i.s.o mov_s r0,128 */
 /* { dg-final { scan-assembler-times "mov_s\\s+r0,128" 2 } } */
 /* { dg-final { scan-assembler "mov\\s+r0,-33" } } */
-/* { dg-final { scan-assembler-times "stb\\s+r0,\\\[@mem\\\]" 5 } } */
+/* { dg-final { scan-assembler-times "st\\s+r0,\\\[@mem\\\]" 5 } } */
