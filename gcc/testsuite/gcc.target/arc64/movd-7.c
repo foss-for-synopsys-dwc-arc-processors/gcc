44fbc662c0cf5121737942c483c325f17e61ea19
Shahab Vahedi <shahab@synopsys.com>
2019-09-30 15:16:29 +0200

    [MD] Add movdi along with the tests
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

---
+++ gcc/testsuite/gcc.target/arc64/movd-7.c
@@ -0,0 +1,14 @@
+/* { dg-do compile } */
+/* { dg-options "-O1 -fomit-frame-pointer" } */
+
+/* check "movd-1.c" for further details. */
+
+/* assign memory to a memory */
+int mem_dst, mem_src;
+void foo(void)
+{
+  mem_dst = mem_src;
+}
+/* { dg-final { scan-assembler "ld\\s+r\[0-9\]+,\\\[@mem_src\\\]" } } */
+/* { dg-final { scan-assembler-not "ext\[bhw\]\\s+" } } */
+/* { dg-final { scan-assembler "st\\s+r0,\\\[@mem_dst\\\]" } } */
