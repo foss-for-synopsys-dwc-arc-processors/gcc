44411cf6405f1ffee58fffca82723d6df9a29e56
Shahab Vahedi <shahab@synopsys.com>
2019-09-30 10:44:19 +0200

    [MD] Add movhi along with tests.
    Signed-off-by: Shahab Vahedi <shahab@synopsys.com>

--- gcc/testsuite/gcc.target/arc64/movh-5.c
+++ gcc/testsuite/gcc.target/arc64/movh-5.c
@@ -1,11 +1,11 @@
 /* { dg-do compile } */
 /* { dg-options "-O0 -fomit-frame-pointer" } */
 
-/* check "movq-1.c" for further details. */
+/* check "movh-1.c" for further details. */
 
 /* assign immediate to a memory: this immediate is small *
  * enough to be covered by w6 (signed 6 bit number).     */
-char mem;
+short mem;
 void foo(void)
 {
   mem = 0x00;    /* the usual suspect: 0 */
@@ -13,7 +13,7 @@ void foo(void)
   mem = -32;     /* smallest negative number in w6 */
   mem = -1;      /* just a -1 (all bits 1) */
 }
-/* { dg-final { scan-assembler "stb\\s+0,\\\[@mem\\\]" } } */
-/* { dg-final { scan-assembler "stb\\s+31,\\\[@mem\\\]" } } */
-/* { dg-final { scan-assembler "stb\\s+-32,\\\[@mem\\\]" } } */
-/* { dg-final { scan-assembler "stb\\s+-1,\\\[@mem\\\]" } } */
+/* { dg-final { scan-assembler "sth\\s+0,\\\[@mem\\\]" } } */
+/* { dg-final { scan-assembler "sth\\s+31,\\\[@mem\\\]" } } */
+/* { dg-final { scan-assembler "sth\\s+-32,\\\[@mem\\\]" } } */
+/* { dg-final { scan-assembler "sth\\s+-1,\\\[@mem\\\]" } } */
