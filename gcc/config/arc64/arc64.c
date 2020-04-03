a97a5f89c54590f0835d1fa1337dd1011eab9bbf
Claudiu Zissulescu <claziss@gmail.com>
2020-04-03 11:23:45 +0300

    Low nibble needs to be unsigned.

--- gcc/config/arc64/arc64.c
+++ gcc/config/arc64/arc64.c
@@ -940,7 +940,7 @@ arc64_print_operand (FILE *file, rtx x, int code)
 	}
       ival = INTVAL (x);
       ival &= 0xffffffffULL;
-      fprintf (file, "%d", (int32_t) ival);
+      fprintf (file,"0x%08" PRIx32, (uint32_t) ival);
       break;
 
     case 'h':
