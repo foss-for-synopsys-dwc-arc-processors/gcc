be28797ca990390fff8bc0c09df18fac0f9285c1
Claudiu Zissulescu <claziss@synopsys.com>
2021-04-05 19:58:03 +0300

    arc64: Add int SIMD vec_shl, fix fp SIMD vec_extract

--- gcc/config/arc64/arc64-protos.h
+++ gcc/config/arc64/arc64-protos.h
@@ -38,6 +38,7 @@ extern void arc64_expand_casesi (rtx []);
 extern bool arc64_simd64x_split_move_p (rtx *, machine_mode);
 extern void arc64_simd128_split_move (rtx *, machine_mode);
 extern unsigned arc64_dbx_register_number (unsigned);
+extern bool arc64_expand_fvect_shr (rtx *);
 #endif /* RTX_CODE */
 
 #endif /* GCC_ARC64_PROTOS_H */
