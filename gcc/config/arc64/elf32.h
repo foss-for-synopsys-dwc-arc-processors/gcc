bc51db4423f342bfe95ae79ea27fad98a064f384
Claudiu Zissulescu <claziss@synopsys.com>
2021-12-07 10:08:33 +0200

    arc32: Add new ARCv3:32 triplet

--- gcc/config/arc64/elf32.h
+++ gcc/config/arc64/elf32.h
@@ -18,21 +18,13 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#undef DWARF2_UNWIND_INFO
-#define DWARF2_UNWIND_INFO 0
+#undef  LINK_SPEC
+#define LINK_SPEC "%{h*} \
+   %{static:-Bstatic}				\
+   %{shared:-shared}				\
+   %{symbolic:-Bsymbolic}			\
+   %{!static:%{rdynamic:-export-dynamic}}	\
+   %{mcpu=hs6*:-m arc64elf64}"
 
-/* If no specs file is enforced, default to nosys libarary.  */
-#undef LINK_GCC_C_SEQUENCE_SPEC
-#define LINK_GCC_C_SEQUENCE_SPEC				\
-  "--start-group %G %{!specs=*:%{!nolibc:-lc -lnosys}} --end-group"
-
-/* Make sure we include the crtbegin.o.  */
-#undef STARTFILE_SPEC
-#define STARTFILE_SPEC "crt0%O%s crti%O%s crtbegin%O%s"
-
-/* ...and crtend.o.  */
-#undef ENDFILE_SPEC
-#define ENDFILE_SPEC "crtend%O%s crtn%O%s"
-
-#undef UNALIGNED_ACCESS_DEFAULT
-#define UNALIGNED_ACCESS_DEFAULT 1
+#undef ARC64_64BIT_DEFAULT
+#define ARC64_64BIT_DEFAULT 0
