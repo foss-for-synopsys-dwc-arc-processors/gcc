1b7f27f4aa2428484e21a857b8a9dd30a8d94f42
Shahab Vahedi <shahab@synopsys.com>
2021-12-14 11:15:05 +0100

    arc32: add lnx target triplet

--- gcc/config/arc64/linux64.h
+++ gcc/config/arc64/linux64.h
@@ -1,4 +1,4 @@
-/* Target macros for arc32-elf targets.
+/* Target macros for arc64-*-linux targets.
 
    Copyright (C) 2021 Free Software Foundation, Inc.
 
@@ -18,13 +18,20 @@ You should have received a copy of the GNU General Public License
 along with GCC; see the file COPYING3.  If not see
 <http://www.gnu.org/licenses/>.  */
 
-#undef  LINK_SPEC
+#define GLIBC_DYNAMIC_LINKER "/lib/ld-linux-arc64.so.2"
+
+/* Note that the default is to link against dynamic libraries, if they are
+   available.  Override with -static.  */
+#undef LINK_SPEC
 #define LINK_SPEC "%{h*} \
-   %{static:-Bstatic}				\
-   %{shared:-shared}				\
-   %{symbolic:-Bsymbolic}			\
-   %{!static:%{rdynamic:-export-dynamic}}	\
-   %{mcpu=hs6*:-m arc64elf64}"
+  %{static:-Bstatic}					    \
+  %{shared:-shared}					    \
+  %{symbolic:-Bsymbolic}				    \
+  %{!static:						    \
+    %{rdynamic:-export-dynamic}				    \
+    %{!shared:-dynamic-linker " GNU_USER_DYNAMIC_LINKER "}} \
+  %{mcpu=hs5*:-m arc64linux32}				    \
+  -X "
 
 #undef ARC64_64BIT_DEFAULT
-#define ARC64_64BIT_DEFAULT 0
+#define ARC64_64BIT_DEFAULT 1
