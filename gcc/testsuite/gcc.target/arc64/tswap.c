804d833d3698d94151052e27654647e7f807c921
Claudiu Zissulescu <claziss@synopsys.com>
2022-02-22 13:06:33 +0200

    arc64: Update vect_perm cost.

--- gcc/testsuite/gcc.target/arc64/tswap.c
+++ gcc/testsuite/gcc.target/arc64/tswap.c
@@ -1,6 +1,9 @@
 /* { dg-do compile } */
+/* { dg-require-effective-target hs6x } */
 /* { dg-options "-O2 -msimd -ftree-vectorize" } */
 
+/* Enable this test when HS5x recognizes various vector permutations
+   operations.  */
 
 struct{
   unsigned short x1;
@@ -12,4 +15,4 @@ void foo (void)
   vara.x1 = varb.x2;
   vara.x2 = varb.x1;
 }
-/* { dg-final { scan-assembler "swap" } } */
+/* { dg-final { scan-assembler "swap\\s+r\\d+,r\\d+" } } */
